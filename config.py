18395b95394677b8d633092c0cb546077229c28e
projectApelsin <copies.everywhere@gmail.com>
2025-02-23 20:45:28 +0200

    refactoring

--- config.py
+++ config.py
@@ -3,16 +3,19 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 
-# Получаем строку JSON из переменной окружения
-cred_json_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+# Загружаем BOT_TOKEN из переменных окружения
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not found in environment variables.")
 
+# Загружаем Firebase credentials из переменной окружения
+cred_json_str = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
 if not cred_json_str:
     raise ValueError("Google credentials not found in environment variable.")
 
-# Преобразуем строку JSON в словарь
-cred_json = json.loads(cred_json_str)
+cred_dict = json.loads(cred_json_str)
+cred = credentials.Certificate(cred_dict)
 
-# Проверяем, не инициализирован ли Firebase уже
+# Инициализируем Firebase (гарантируем, что вызываем только один раз)
 if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_json)
     firebase_admin.initialize_app(cred)
