1f8ed1dc1bdd6c924bdae55ba8046de7a9f4fd4d
projectApelsin <copies.everywhere@gmail.com>
2025-02-23 20:12:02 +0200

    refactoring ``

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import os
-from dotenv import load_dotenv
+import json
+import firebase_admin
+from firebase_admin import credentials
 
-load_dotenv(dotenv_path='d:/projects/telegram-bot-notes/.env')
+# Получаем строку JSON из переменной окружения
+cred_json_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+if cred_json_str is None:
+    raise ValueError("Google credentials not found in environment variable.")
 
-print(BOT_TOKEN)  # Перевір, чи правильно завантажено токен
-print(GOOGLE_APPLICATION_CREDENTIALS) 
+# Преобразуем строку JSON в словарь
+cred_json = json.loads(cred_json_str)
+
+# Инициализируем Firebase с использованием полученных данных
+cred = credentials.Certificate(cred_json)
+firebase_admin.initialize_app(cred)
